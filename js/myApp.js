0f2c29685a6840fcea4ced64ffde687911e7e26b
Shawn Stallings <oxegeneral@yahoo.com>
2015-10-20 01:11:33 -0600

    started angular coding

--- js/myApp.js
+++ js/myApp.js
@@ -1,5 +1,27 @@
-var app = angular.module('myApp', ['ngAnimate']);
+var app = angular.module('myApp', []);
 
-app.controller('MyCtrl', function($scope) {
+app.controller('MyCtrl', function($scope, $http) {
+	
 
-})
+	$scope.submit = function() {
+
+		var url = 'https://api.instagram.com/v1/tags/red/media/recent';
+		var request = {
+			callback: 'JSON_CALLBACK',
+			client_id: '60cb16fb5fa2446cb4e25add2fd87c94'
+		};
+		$http({
+			method: 'JSONP',
+			url: url,
+			params: request			
+		})
+		.success(function(data) {
+			console.log(data);
+		})
+		.error(function() {
+			console.log("error no data");
+			//create error message
+		})
+	};
+
+});
